3021c9fdb8583c16e18fa9f8f47229a7dce4b21e
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2025-11-06 16:20:31 +0200

    implemented suggestions empty and header

--- src/app/suggestions/suggestions.ts
+++ src/app/suggestions/suggestions.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './suggestions.scss',
 })
 export class Suggestions {
-
+  bulbTwoImage: string = 'assets/bulb-2.png';
+  arrowDown: string = 'assets/arrow.svg';
+  magnifying: string = 'assets/person-magnifying.png';
 }
